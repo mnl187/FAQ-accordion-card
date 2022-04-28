939120996f3d46d09abe4841139f9e7d68197e53
mnl187 <mnl187@gmail.com>
2022-04-28 19:27:37 +0200

    finished mobile

---
+++ script.js
@@ -0,0 +1,10 @@
+    let items = document.querySelectorAll('.according-item');
+
+    items.forEach(item => item.addEventListener('click', ()=> {
+        console.log('działa');
+    }));
+// const item = document.querySelector('.box-according');
+//
+// item.addEventListener('click', ()=> {
+//     console.log('działa');
+// });
